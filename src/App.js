e063e6ccea70071b8a30b6299c3e679e3fac5019
rociac <brsrkdev@gmail.com>
2020-05-11 17:01:49 -0500

    Add testing library, Add basic redux setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import React from 'react';
+import List from './components/List';
 
 function App() {
-  return <div className='App'>Hello World</div>;
+  return (
+    <div className='App'>
+      <List />
+    </div>
+  );
 }
 
 export default App;
